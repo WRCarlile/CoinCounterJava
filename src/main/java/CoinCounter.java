33f8b9f305403b49e4a87e1fd4883b3b23f8903c
Blake Bless and Ryan Carlile <Guest@epicodus-31.local>
2016-04-21 15:22:36 -0700

    added all elements to page, with some css.

--- src/main/java/CoinCounter.java
+++ src/main/java/CoinCounter.java
@@ -1,30 +1,39 @@
 import java.util.*;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.ArrayList;
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
-// import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
 public class CoinCounter {
   public static void main(String[] args) {
-    // get("/", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/home.vtl");
-    //   return new ModelAndView(model, "templates/layout.vtl");
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/detector", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //
-    //   String userInput = request.queryParams("number");
-    //   CoinCounter newCount = new CoinCounter();
-    //   String count = newCount.coinCombo(userInput);
-    //   model.put("count", count);
-    //
-    //   model.put("template", "templates/detector.vtl");
-    //   return new ModelAndView(model, "templates/layout.vtl");
-    // }, new VelocityTemplateEngine());
+    staticFileLocation("/public");
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
+
+    get("/detector", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      String userInput = request.queryParams("coin");
+      Integer inputVar = Integer.parseInt(userInput);
+      CoinCounter newCount = new CoinCounter();
+      Integer[] count = newCount.coinCombo(inputVar);
+      Integer quarters = count[0];
+      Integer dimes = count[1];
+      Integer nickels = count[2];
+      Integer pennies = count[3];
+
+      model.put("quarters", quarters);
+      model.put("dimes", dimes);
+      model.put("nickels", nickels);
+      model.put("pennies", pennies);
+
+      model.put("template", "templates/detector.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
   }
 
   public static Integer[] coinCombo(Integer inputVar) {
@@ -43,22 +52,20 @@ public class CoinCounter {
       inputVar = inputVar - (total[0] * quarter);
     }
 
-System.out.println(inputVar);
     while(inputVar >= 10 && inputVar < 25){
       total[1] = inputVar / dime;
       inputVar = inputVar - (total[1] * dime);
     }
-System.out.println(inputVar);
+
     while(inputVar >= 5 && inputVar < 10){
       total[2] = inputVar / nickel;
       inputVar = inputVar - (total[2] * nickel);
     }
-System.out.println(inputVar);
+
     while(inputVar >= 1 && inputVar < 5){
       total[3] = inputVar / penny;
       inputVar = inputVar - (total[3] * penny);
     }
-System.out.println(total);
     return total;
   }
 }
